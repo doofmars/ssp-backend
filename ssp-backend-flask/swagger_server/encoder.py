933b1fab16b8a44815ef94786628aa625ebcf360
doofmars <git@preuss.in>
2023-12-09 10:10:19 +0100

    Fix server setup
    Server is unable to start otherwise

--- ssp-backend-flask/swagger_server/encoder.py
+++ ssp-backend-flask/swagger_server/encoder.py
@@ -1,10 +1,10 @@
-from connexion.apps.flask_app import FlaskJSONEncoder
+from connexion.frameworks.flask import FlaskJSONProvider
 import six
 
 from swagger_server.models.base_model_ import Model
 
 
-class JSONEncoder(FlaskJSONEncoder):
+class JSONEncoder(FlaskJSONProvider):
     include_nulls = False
 
     def default(self, o):
@@ -17,4 +17,4 @@ class JSONEncoder(FlaskJSONEncoder):
                 attr = o.attribute_map[attr]
                 dikt[attr] = value
             return dikt
-        return FlaskJSONEncoder.default(self, o)
+        return FlaskJSONProvider.default(self, o)
