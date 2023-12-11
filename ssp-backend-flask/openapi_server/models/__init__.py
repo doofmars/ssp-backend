dca38318c99920284a2a861cc6ae83fe7eed4c40
doofmars <git@preuss.in>
2023-12-11 20:15:01 +0100

    Cleanup

--- ssp-backend-flask/openapi_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-# import models into model package
-from openapi_server.models.board import Board
-from openapi_server.models.create_game import CreateGame
-from openapi_server.models.game import Game
-from openapi_server.models.game_game_id_turn_post_request import GameGameIdTurnPostRequest
-from openapi_server.models.game_status import GameStatus
-from openapi_server.models.pawn import Pawn
-from openapi_server.models.player import Player
