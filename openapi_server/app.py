c61e2a113f566b30c2508b4dfab80f5b6451bbd3
doofmars <git@preuss.in>
2023-12-13 11:20:28 +0100

    Allow flask to run the app

--- openapi_server/app.py
+++ openapi_server/app.py
@@ -5,6 +5,8 @@ from flask import render_template  # Remove: import Flask
 import psycopg2
 import connexion
 
+from openapi_server.init_db import init_db
+
 app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
 
 app.add_api("openapi.yaml")
@@ -15,15 +17,8 @@ def home():
     return render_template("index.html")
 
 
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.environ['DB_HOST'],
-        database='ssp_db',
-        user=os.environ['DB_USERNAME'],
-        password=os.environ['DB_PASSWORD']
-    )
-    return conn
-
+init_db()
+app.run(host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
