21fd92f576dd7d5620bf8a18274285e451b6a6f8
doofmars <git@preuss.in>
2023-12-16 21:30:11 +0100

    Allow all cors origins in development env

--- openapi_server/factory.py
+++ openapi_server/factory.py
@@ -10,14 +10,17 @@ def start_server():
     app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
     app.add_api("openapi.yaml")
 
-    # app.add_middleware(
-    #     CORSMiddleware,
-    #     position=MiddlewarePosition.BEFORE_EXCEPTION,
-    #     allow_origins=["*"],
-    #     allow_credentials=True,
-    #     allow_methods=["*"],
-    #     allow_headers=["*"],
-    # )
+    print("Running in FLASK_ENV: " + str(os.environ.get("FLASK_ENV")))
+
+    if os.environ.get("FLASK_ENV") == "development":
+        app.add_middleware(
+            CORSMiddleware,
+            position=MiddlewarePosition.BEFORE_EXCEPTION,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
     # Check for required environment variable DB_USERNAME DB_PASSWORD and DB_HOST and fail to start if not set
     if not os.environ.get("DB_USERNAME"):
